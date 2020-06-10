94684f2e88b62695a5d7c31ba5d37028bef8689c
Praveen Balanagendra <praveen_balanagendra@infosys.com>
2020-06-10 14:08:17 +0530

    Demo - Multiple containers with the same image behind load balancer

---
+++ Demo3/services/greetings/Controllers/greetings.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace greetings.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class GreetingController : ControllerBase
+    {
+        [HttpGet("{name}")]
+        public IActionResult Get(string name)
+        {
+            return Ok("Welcome " + name);
+        }
+    }
+}
